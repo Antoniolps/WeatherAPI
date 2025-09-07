52f891efc7da6385f3f04b598ffcdb9d1ce1792d
antoniolps <antoniolps218@gmail.com>
2025-09-07 13:39:03 -0300

    ✨ feat: configure weather API URL in application properties

--- src/main/java/br/com/antoniolps/WeatherAPI/infra/client/WeatherClient.java
+++ src/main/java/br/com/antoniolps/WeatherAPI/infra/client/WeatherClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "weatherClient", url = "https://api.openweathermap.org/data/2.5", fallback = WeatherClientFallback.class)
+@FeignClient(name = "weatherClient", url = "${weather.api.url}", fallback = WeatherClientFallback.class)
 public interface WeatherClient {
 
     @GetMapping("/weather")
